aae466af0aaf1b95423e528a2961947aafca75ea
Emir <emiratesbp@gmail.com>
2015-05-06 13:11:24 +0200

    first commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="870 111th Ave. N. Suite 3"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Naples Florida, 34108";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2395970190;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="pop"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
